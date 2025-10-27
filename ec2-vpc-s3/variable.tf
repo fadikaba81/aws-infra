eca5f3f034beb37c0e6b8d6f101474b699245404
Fadi Kaba <fadi.cloud.kaba@gmail.com>
2025-10-27 18:47:50 +1100

    Rename the folder

---
+++ ec2-vpc-s3/variable.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-2"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "environment" {
+  description = "The name of the environment"
+  type        = string
+  default     = "Dev"
+
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.250.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used Variables Subnet"
+  type        = string
+  default     = "ap-southeast-2a"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assignment for Variables Subnet"
+  type        = bool
+  default     = true
+}
